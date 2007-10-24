01b3ede8640bf5738167b95d92a9c0896ab2e569
asartalo <asartalo@5ebe07e0-3b37-0410-b882-833e1ed5e216>
2007-10-24 02:38:35 +0000

    Preparation for Branching

--- Asar/Template/Renderer.php
+++ Asar/Template/Renderer.php
@@ -1,7 +1,14 @@
 <?php
-
-class Asar_Template_Renderer {
+class Asar_Template_Renderer  extends Asar_Base {
+  private $template_params = array();
   
+  function setTemplateParams(array $params) {
+    $this->template_params = $params;
+  }
+  
+  function getTemplateParams() {
+    return $this->template_params;
+  }
 }
 
 
