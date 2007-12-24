9ab860df9173f9efc368b8c84e717d02324fe6d4
asartalo <asartalo@5ebe07e0-3b37-0410-b882-833e1ed5e216>
2007-12-24 08:08:11 +0000

    Continued with Default Client

--- Asar/Client/Default.php
+++ Asar/Client/Default.php
@@ -3,11 +3,31 @@
 class Asar_Client_Default extends Asar_Client {
 	
 	function createRequest() {
-		return parent::createRequest($_SERVER['REQUEST_URI'],
+ 		
+		return parent::createRequest( $this->getUriFromServerVars(),
 	    	array(
 	    		'method' => $_SERVER['REQUEST_METHOD']
 	    	)
 		);
+	}
+	
+	/**
+	 * Tries to obtain the uri from the $_SERVER global variable
+	 *
+	 * @return string URI 
+	 **/
+	private function getUriFromServerVars()
+	{
+		if (array_key_exists('REDIRECT_URL', $_SERVER)) {
+ 			return $_SERVER['REDIRECT_URL'];
+ 		} else {
+			$qrstr_start = strpos($_SERVER['REQUEST_URI'], '?');
+			if ($qrstr_start > 0) {
+				return substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], '?'));
+			} else {
+				return $_SERVER['REQUEST_URI'];
+			}
+		}
 	}
 }
 
--- tests/unit/Asar/Client/DefaultTest.php
+++ tests/unit/Asar/Client/DefaultTest.php
@@ -9,7 +9,8 @@ class Asar_Client_DefaultTest extends Asar_Test_Helper {
 	{
 		$this->client = new Asar_Client_Default;
 		
-		$_SERVER['REQUEST_URI'] = '/basic/var1/var2.txt?enter=true$center=1&stupid&crazy=beautiful';
+		$_SERVER['REQUEST_URI'] = '/basic/var1/var2.txt?enter=true&center=1&stupid&crazy=beautiful';
+		$_SERVER['QUERY_STRING'] = 'enter=true&center=1&stupid&crazy=beautiful';
 	    $_SERVER['REQUEST_METHOD'] = 'GET';
 	    $_GET['enter']  = 'true';
 	    $_GET['center'] = '1';
@@ -27,14 +28,29 @@ class Asar_Client_DefaultTest extends Asar_Test_Helper {
 	function testSendRequest() {
     	$request = $this->client->createRequest();
 	    $this->assertEquals('Asar_Request', get_class($request), 'Invalid object type. Must be \'Asar_Request\'.');
-	    //$this->assertEquals($expected_params, $request->getParams(), 'Unable to get params');
-	    //$this->assertEquals('txt', $request->getType(), 'Unable to get type');
 	}
 	
 	function testSendRequestReadsServerRequestMethod() {
 		$_SERVER['REQUEST_METHOD'] = 'POST';
 		$request = $this->client->createRequest();
 		$this->assertEquals(Asar_Request::POST, $request->getMethod(), 'Method mismatch');
+	}
+	
+	function testSendRequestGetsUriFromRequestUriButSubtractTheQueryString() {
+		$request = $this->client->createRequest();
+		$this->assertEquals('/basic/var1/var2.txt', $request->getUri(), 'Unable to set Uri Properly');
+	}
+	
+	function testSendRequestGetsUriFromRequestUri() {
+		$_SERVER['REQUEST_URI'] = '/basic/var1/var3.txt';
+		$request = $this->client->createRequest();
+		$this->assertEquals('/basic/var1/var3.txt', $request->getUri(), 'Unable to set Uri Properly');
+	}
+	
+	function testSendRequestReadsRedirectUrlWhenAvailable() {
+		$_SERVER['REDIRECT_URL'] = '/funny/';
+		$request = $this->client->createRequest();
+		$this->assertEquals('/funny/', $request->getUri(), 'Unable to set Uri Properly');
 	}
 }
 ?>
