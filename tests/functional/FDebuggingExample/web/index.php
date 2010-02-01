380c48d8952c8e862a69a35711728f725a3137b3
asartalo <asartalo@5ebe07e0-3b37-0410-b882-833e1ed5e216>
2010-02-01 19:50:53 +0000

    Added missed files for testing tests/functional/FDebuggingExample/web.

---
+++ tests/functional/FDebuggingExample/web/index.php
@@ -0,0 +1,10 @@
+<?php
+// TODO: Use Asar_Utility_ServerSetup instead
+set_include_path(
+    realpath(dirname(__FILE__) .'/../../../../core') . PATH_SEPARATOR .
+    realpath(dirname(__FILE__) . '/..') . PATH_SEPARATOR .
+    get_include_path()
+);
+require_once 'Asar.php';
+Asar::setMode(Asar::MODE_DEBUG);
+Asar::start('DebuggingExample');
