f0f4f59c9a6c99020335e2c9a6a41a07be605f88
asartalo <asartalo@5ebe07e0-3b37-0410-b882-833e1ed5e216>
2010-02-08 15:24:56 +0000

    Asar_Resource preliminary refactoring.

--- core/Asar/Resource.php
+++ core/Asar/Resource.php
@@ -38,11 +38,14 @@ class Asar_Resource implements Asar_Requestable {
       $this->template = new $this->template_engine;
       return $this->template;
     }
-    //$trace = debug_backtrace();
     trigger_error(
       "Unknown property '$property' via __get()",
       E_USER_WARNING
     );
+  }
+  
+  private function _runMethod() {
+    return call_user_func(array($this, $this->request->getMethod()));
   }
   
   function handleRequest(Asar_Request_Interface $request) {
@@ -52,39 +55,8 @@ class Asar_Resource implements Asar_Requestable {
       if ($request->getMethod() === 'POST') {
         $_POST = $this->request->getContent();
       }
-      $content_type = $this->contentNegotiate($request->getHeader('Accept'));
-      if (!array_key_exists($content_type, self::$_types)) {
-        $this->response->setStatus(406);
-      }
-      $content = call_user_func(array($this, $request->getMethod()));
-      if (!$content && isset($this->template)) {
-        if (!$this->template->getTemplateFile()) {
-          // Infer template file based on name of context and this resource 
-          $this->template->setTemplateFile(
-            $this->constructTemplateFilePath()
-          );
-        }
-        if (array_key_exists('default_representation_dir', $this->config)) {
-          $this->template->setLayout( Asar::constructPath(
-            $this->config['default_representation_dir'],
-            'Layout' . $this->getTemplateTypeToUse() . '.php'
-          ));
-        }
-        try {
-          $content = $this->template->render();
-          // TODO: Create more functional tests for content negotiation
-          $this->response->setHeader( 'Content-Type', $content_type );
-        } catch (Asar_Template_Exception_FileNotFound $e) {
-          $this->response->setStatus(406);
-        }
-      }
-      $this->response->setContent($content);
-      if (!$this->response->getStatus()) {
-        $this->response->setStatus(200);
-      }
-      if (!$this->response->getHeader('Content-Type')) {
-        $this->response->setHeader('Content-Type', 'text/html');
-      }
+      $this->response->setContent($this->_getContent($request));
+      $this->_setResponseDefaults();
     } catch (Exception $e) {
       $this->response->setContent($e->getMessage());
       $this->response->setStatus(500);
@@ -92,7 +64,28 @@ class Asar_Resource implements Asar_Requestable {
     return $this->response;
   }
   
-  function contentNegotiate($accept) {
+  private function _setResponseDefaults() {
+    if (!$this->response->getStatus()) {
+      $this->response->setStatus(200);
+    }
+    if (!$this->response->getHeader('Content-Type')) {
+      $this->response->setHeader('Content-Type', 'text/html');
+    }
+  }
+  
+  private function _getContent($request) {
+    $content_type = $this->_contentNegotiate($request->getHeader('Accept'));
+    if (!$content_type) {
+      $this->response->setStatus(406);
+    }
+    $content = $this->_runMethod();
+    if (!$content && isset($this->template)) {
+      return $this->_renderTemplate($content_type);
+    }
+    return $content;
+  }
+  
+  protected function _contentNegotiate($accept) {
     if (array_key_exists($accept, self::$_types)) {
       return $accept;
     }
@@ -105,13 +98,37 @@ class Asar_Resource implements Asar_Requestable {
     return null;
   }
   
-  protected function constructTemplateFilePath() {
+  private function _renderTemplate($content_type) {
+    if (!$this->template->getTemplateFile()) {
+      // Infer template file based on name of context and this resource 
+      $this->template->setTemplateFile(
+        $this->_constructTemplateFilePath()
+      );
+    }
+    if (array_key_exists('default_representation_dir', $this->config)) {
+      $this->template->setLayout( Asar::constructPath(
+        $this->config['default_representation_dir'],
+        'Layout' . $this->_getTemplateTypeToUse() . '.php'
+      ));
+    }
+    try {
+      $content = $this->template->render();
+      // TODO: Create more functional tests for content negotiation
+      $this->response->setHeader( 'Content-Type', $content_type );
+    } catch (Asar_Template_Exception_FileNotFound $e) {
+      $this->response->setStatus(406);
+      $content = '';
+    }
+    return $content;
+  }
+  
+  protected function _constructTemplateFilePath() {
     if (array_key_exists('context', $this->config)) {
       $contextname = str_replace('_Application', '', get_class($this->config['context']));
       $thisname = str_replace( '_', DIRECTORY_SEPARATOR,
         substr_replace(get_class($this), '', 0, strlen($contextname) + 10)
       );
-      $suffix = $this->request->getMethod() . $this->getTemplateTypeToUse() . '.php';
+      $suffix = $this->request->getMethod() . $this->_getTemplateTypeToUse() . '.php';
       $path = Asar::constructPath(
       $this->config['default_representation_dir'],
         $thisname . '.' . $suffix
@@ -126,8 +143,8 @@ class Asar_Resource implements Asar_Requestable {
     return false;
   }
   
-  protected function getTemplateTypeToUse() {
-    $accept = $this->contentNegotiate($this->request->getHeader('Accept'));
+  protected function _getTemplateTypeToUse() {
+    $accept = $this->_contentNegotiate($this->request->getHeader('Accept'));
     if (array_key_exists($accept, self::$_types)) {
       return self::$_types[$accept];
     }
