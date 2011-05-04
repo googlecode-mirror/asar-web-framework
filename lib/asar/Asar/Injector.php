ae25e8bdfdfbd560721d59288524a4ecc70bed14
asartalo <asartalo@5ebe07e0-3b37-0410-b882-833e1ed5e216>
2011-05-04 16:09:21 +0000

    Cleaned up Injector code moving unused methods. This resolves issue #50.

--- lib/asar/Asar/Injector.php
+++ lib/asar/Asar/Injector.php
@@ -26,15 +26,6 @@ class Injector {
     return $env_scope->getAppName();
   }
   
-  static function injectEnvironmentHelperCli(EnvironmentScope $env_scope) {
-    return new EnvironmentHelper\Cli(
-      self::injectCli($env_scope),
-      self::injectArgv($env_scope),
-      self::injectInitialTaskLists($env_scope),
-      self::injectCliTaskFileLoader($env_scope)
-    );
-  }
-  
   static function injectClassLoader(EnvironmentScope $env_scope) {
     return new ClassLoader;
   }
@@ -73,71 +64,8 @@ class Injector {
     return new FileIncludeManager;
   }
   
-  static function injectCli(EnvironmentScope $env_scope) {
-    if (!self::$cli) {
-      self::$cli =  new Utility\Cli(
-        self::injectCliInterpreter($env_scope), 
-        self::injectCliExecutor($env_scope),
-        self::injectCurrentWorkingDirectory($env_scope)
-      );
-    }
-    return self::$cli;
-  }
-  
-  static function injectCliInterpreter(EnvironmentScope $env_scope) {
-    return new Utility\Cli\Interpreter;
-  }
-  
-  static function injectCliExecutor(EnvironmentScope $env_scope) {
-    return new Utility\Cli\Executor;
-  }
-  
-  static function injectArgv(EnvironmentScope $env_scope) {
-    return $env_scope->getArgv();
-  }
-  
-  static function injectCurrentWorkingDirectory(
-    EnvironmentScope $env_scope
-  ) {
-    return $env_scope->getCurrentWorkingDirectory();
-  }
-  
-  static function injectInitialTaskLists(EnvironmentScope $env_scope) {
-    return array(
-      self::injectUtilityCliBaseTasks($env_scope),
-      self::injectUtilityCliFrameworkTasks($env_scope)
-    );
-  }
-  
-  static function injectUtilityCliBaseTasks(EnvironmentScope $env_scope) {
-    return new Utility\Cli\BaseTasks;
-  }
-
-  static function injectUtilityCliFrameworkTasks(
-    EnvironmentScope $env_scope
-  ) {
-    return new Utility\Cli\FrameworkTasks(
-      self::injectFileHelper($env_scope)
-    );
-  }
-  
-  static function injectFileHelper(EnvironmentScope $env_scope) {
-    return new FileHelper;
-  }
-  
   static function injectDefaultConfig(EnvironmentScope $env_scope) {
     return new Config\DefaultConfig;
   }
   
-  static function injectCliTaskFileLoader(EnvironmentScope $env_scope) {
-    return new Utility\Cli\TaskFileLoader(
-      $env_scope->getCurrentWorkingDirectory(),
-      self::injectUtilityClassFilePeek($env_scope),
-      self::injectCli($env_scope)
-    );
-  }
-  
-  static function injectUtilityClassFilePeek(EnvironmentScope $env_scope) {
-    return new Utility\ClassFilePeek;
-  }
 }
